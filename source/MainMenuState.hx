e3c5009bde42690f2fd056cac64a2b577816d541
Sebaxddd <104731760+Sebaxddd@users.noreply.github.com>
2022-05-01 13:55:56 -0400

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -34,11 +34,8 @@ class MainMenuState extends MusicBeatState
 	
 	var optionShit:Array<String> = [
 		'story_mode',
-		'freeplay',
-		#if MODS_ALLOWED 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
+		'freeplay'
 		'credits',
-		#if !switch 'donate', #end
 		'options'
 	];
 
